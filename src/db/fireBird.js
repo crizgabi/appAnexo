434bd601f48286606f55dfc43e1ce669a718f7ce
Gabi <crizgabi@hotmail.com>
2025-09-30 12:12:14 -0300

    connection to db

---
+++ src/db/fireBird.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import firebird from "node-firebird";
+
+
+const options = {
+    host: process.env.FB_HOST,
+    port: process.env.FB_PORT,
+    database: process.env.FB_DATABASE,
+    user: process.env.FB_USER,
+    password: process.env.FB_PASSWORD,
+    lowercase_keys: false,
+    role: null,
+    pageSize: 4096,
+};
+
+export function getConnection(callback) {
+  firebird.attach(options, (err, db) => {
+    if (err) {
+      console.error("Erro ao conectar:", err);
+      callback(err, null);
+    } else {
+      callback(null, db);
+    }
+  });
+}
