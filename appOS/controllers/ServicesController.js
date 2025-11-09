fdc5435c9d9a631a82ef485ad7f4130ee2b1eb65
Gabi <crizgabi@hotmail.com>
2025-11-09 14:42:09 -0300

    services working

--- appOS/controllers/ServicesController.js
+++ appOS/controllers/ServicesController.js
@@ -1,35 +1,33 @@
 import { ServicesService } from "../services/ServicesService.js";
 
 export const getServices = async (req, res) => {
-    const { description, reference, name, serviceId } = req.query;
+  const { description, reference, name, serviceId } = req.query;
 
-    try {
-        if (description) {
-            const services = await ServicesService.getServiceByDescription(description);
-            return res.json(services);
-        }
+  try {
+    let services = [];
 
-        if (reference) {
-            const services = await ServicesService.getServiceByReference(reference);
-            return res.json(services);
-        }
-
-        if (name) {
-            const services = await ServicesService.getServiceByName(name);
-            return res.json(services);
-        }
-
-        if (serviceId) {
-            const services = await ServicesService.getServiceByPrimaryKey(serviceId);
-            return res.json(services);
-        }
-
-        return res.status(400).json({ error: "Informe 'description' ou 'reference' na query." });
-
-    } catch (error) {
-        console.error("Erro ao buscar serviços:", error);
-        res.status(500).json({ error: "Erro interno ao buscar serviços." });
+    if (description) {
+      services = await ServicesService.getServiceByDescription(description);
+    } else if (reference) {
+      services = await ServicesService.getServiceByReference(reference);
+    } else if (name) {
+      services = await ServicesService.getServiceByName(name);
+    } else if (serviceId) {
+      services = await ServicesService.getServiceByPrimaryKey(serviceId);
+    } else {
+      return res.status(400).json({ error: "Informe 'descricao', 'referencia', 'nome' ou 'id' na query." });
     }
+
+    if (!services || services.length === 0) {
+      return res.status(404).json({ message: "Nenhum serviço encontrado." });
+    }
+
+    return res.json(services);
+
+  } catch (error) {
+    console.error("Erro ao buscar serviços:", error);
+    res.status(500).json({ error: "Erro interno ao buscar serviços." });
+  }
 };
 
 export const getServiceDetails = async (req, res) => {
@@ -38,7 +36,7 @@ export const getServiceDetails = async (req, res) => {
     try {
         const service = await ServicesService.getServiceDetails(serviceId);
         if (!service) {
-            return res.status(404).json({ error: "Serviço não encontrado." });
+            return res.status(404).json({ message: "Serviço não encontrado." });
         }
         res.json(service);
     } catch (error) {
