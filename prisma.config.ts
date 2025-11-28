36c0fa03ff8a5fafa804194dcd54380f7a76af2d
Gabi <crizgabi@hotmail.com>
2025-11-28 19:26:11 -0300

    prisma alterado para 6.19

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,15 @@
-import 'dotenv/config'
-import { defineConfig, env } from 'prisma/config'
+import { config } from "dotenv";
+import { defineConfig, env } from "prisma/config";
+
+config();
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: 'prisma/migrations',
+    path: "prisma/migrations",
   },
+  engine: "classic",
   datasource: {
-    url: env('DATABASE_URL'),
+    url: env("DATABASE_URL"),
   },
-})
+});
